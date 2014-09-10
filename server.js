4d5211f3c7dc226b1a0a9cc3121f8c774af7de33
paper <jue_paper@126.com>
2014-09-10 19:52:39 +0800

    format server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ function getOptions(page) {
     path: '/ooxx/page-' + page,
     method: 'get',
     headers: {
-      "Accept": "	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
       "Accept-Language": "zh-CN, zh;q=0.8",
       "Cache-Control": "max-age=0",
       "Connection": "Keep-Alive",
       "Host": "jandan.net",
-      "User-Agent": "	Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0"
+      "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0"
     }
   }
 }
